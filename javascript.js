8ef9dcafa856b02d49f89f7f85860ede18c7d84d
andres_lopez <felifit27@gmail.com>
2023-04-23 20:05:47 -0500

    encriptador de textos

--- javascript.js
+++ javascript.js
@@ -4,41 +4,56 @@ let btnEncriptar = document.querySelector('.btn1')
 let btnDesencriptar = document.querySelector('.btn2')
 let btnCopiarTexto = document.querySelector('.btn3')
 
-function encriptarTexto(texto){
+function encriptarTexto(texto) {
     let scripts = ''
-    for(let i = 0;i < texto.length;i++){
-        if(texto[i] === 'a'){
-            scripts += 'ai'
-        }else if(texto[i] === 'e'){
-            scripts += 'enter'
-        }else if(texto[i] === 'i'){
-            scripts += 'imes'
-        }else if(texto[i] === 'o'){
-            scripts += 'ober'
-        }else if(texto[i] === 'u'){
-            scripts += 'ufat'
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === texto[i].toUpperCase()) {
+            alert('No son permitidas las mayusculas.')
+            process.exit()
         }
-        scripts += texto[i]
+        if (texto[i] === 'a') {
+            scripts += 'ai'
+        } else if (texto[i] === 'e') {
+            scripts += 'enter'
+        } else if (texto[i] === 'i') {
+            scripts += 'imes'
+        } else if (texto[i] === 'o') {
+            scripts += 'ober'
+        } else if (texto[i] === 'u') {
+            scripts += 'ufat'
+        } else {
+            scripts += texto[i]
+        }
     }
     return scripts
 }
 
-function escribir(){
+function escribir() {
     let result = encriptarTexto(encriptandoTexto.value)
     textoEncriptado.value = result
 }
 
-function desencriptar(texto){
-    let limpiarTexto = texto.replace(/ai|enter|imes|ober|ufat|\n/g, '')
-    return limpiarTexto
+function desencriptar(text) {
+    const removerLetras = {
+        ai: /i/g,
+        enter: /nter/g,
+        month: /mas/g,
+        ber: /ber/g,
+        fat: /fat/g,
+    };
+    let transformarTxt = text;
+    for (const [pattern, regex] of Object.entries(removerLetras)) {
+        transformarTxt = transformarTxt.replace(regex, "");
+    }
+    return transformarTxt;
 }
 
-function mostrar(){
+function mostrar() {
     let desencrip = desencriptar(textoEncriptado.value)
     textoEncriptado.value = desencrip
 }
 
-function copiandoTexto(){
+function copiandoTexto() {
     let copiando = textoEncriptado
     navigator.clipboard.writeText(copiando.value)
 }
